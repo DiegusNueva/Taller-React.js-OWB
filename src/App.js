69a61d05fbc4cf75af880730a2d97661eef9e73c
Diego Alonso Molina <diegusnueva@gmail.com>
2023-09-11 23:40:22 +0200

    Taller de REACT terminado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, addTodo] = useState([]);
+  function setTodo(todo) {
+    addTodo([...todos, todo]);
+  }
+  function deleteTodo(todo) {
+    var index = todos.indexOf(todo);
+    todos.splice(index, 1);
+    addTodo([...todos]);
+  }
+
+  useEffect(() => {});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" id="input-todo" />
+      <button
+        onClick={() => {
+          setTodo(document.getElementById("input-todo").value);
+        }}
+      >
+        Añadir tarea
+      </button>
+      <ul>
+        {todos.map((todo) => (
+          <div className="todo">
+            <li key={todo}>{todo}</li>
+            <p onClick={() => deleteTodo(todo)}>Eliminar</p>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
